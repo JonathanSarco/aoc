fd0ef724bb1eb1bba406122746785163dd76d437
Jony <jfe.sarco@gmail.com>
2023-05-29 14:47:54 +0200

    add day 5 part 2

--- src/main/kotlin/com/jonivan/aoc/y2021/Day05.kt
+++ src/main/kotlin/com/jonivan/aoc/y2021/Day05.kt
@@ -2,6 +2,7 @@ package com.jonivan.aoc.y2021
 
 import com.jonivan.aoc.utils.ListsUtils
 import com.jonivan.aoc.utils.StringUtils
+import kotlin.math.abs
 
 data class Coordinate(val x: Int, val y: Int)
 
@@ -13,8 +14,15 @@ fun readFileAndMap(): List<Pair<Coordinate, Coordinate>> {
 fun isHorizontal(line: Pair<Coordinate, Coordinate>) = line.first.x == line.second.x
 fun isVertical(line: Pair<Coordinate, Coordinate>): Boolean = line.first.y == line.second.y
 
+fun isDiagonal(line: Pair<Coordinate, Coordinate>): Boolean {
+    if (abs(line.first.x - line.second.x) == abs(line.first.y - line.second.y)) {
+        return true
+    }
+    return false
+}
+
 enum class POSITION {
-    X, Y
+    X, Y, DG_MIN, DG_MAX
 }
 fun getMayorMinorFromCoordinates(position: POSITION, line: Pair<Coordinate, Coordinate>): Pair<Int,Int> {
     return when (position) {
@@ -27,6 +35,17 @@ fun getMayorMinorFromCoordinates(position: POSITION, line: Pair<Coordinate, Coor
             Pair(line.first.x, line.second.x)
         } else {
             Pair(line.second.x, line.first.x)
+        }
+        POSITION.DG_MIN ->
+            if (line.first.x > line.second.x) {
+                Pair(line.first.x, line.first.y)
+            } else {
+                Pair(line.second.x, line.second.y)
+            }
+        POSITION.DG_MAX -> if (line.first.x < line.second.x) {
+            Pair(line.first.x, line.first.y)
+        } else {
+            Pair(line.second.x, line.second.y)
         }
     }
 }
@@ -47,7 +66,29 @@ fun main() {
 
     val response = readFileAndMap()
     response.forEach {
+        println("each: $it")
+        if (isDiagonal(it)) {
+            println("diagonal: $it")
+            var min = getMayorMinorFromCoordinates(POSITION.DG_MAX, it)
+            val max = getMayorMinorFromCoordinates(POSITION.DG_MIN, it)
+
+            println("min: $min, max $max")
+
+            var steps = abs(min.first - max.first);
+            val xStep = if (min.first > max.first) -1 else 1;
+            val yStep = if (min.second > max.second) -1 else 1;
+            coverPoint(board, min.first, min.second)
+            var x = min.first
+            var y = min.second
+            while (steps > 0) {
+                steps--
+                x += xStep;
+                y += yStep;
+                coverPoint(board, x, y)
+            }
+        }
         if (isHorizontal(it)) {
+            println("horizontal: $it")
             val c = getMayorMinorFromCoordinates(POSITION.X, it)
             val xValue = it.first.x
             for (i in c.first..c.second) {
@@ -55,6 +96,7 @@ fun main() {
             }
         }
         if (isVertical(it)) {
+            println("vertical: $it")
             val c = getMayorMinorFromCoordinates(POSITION.Y, it)
             val yValue = it.first.y
             for (i in c.first..c.second) {
@@ -65,13 +107,10 @@ fun main() {
 
     var sum = 0
     board.forEach {
-        print("x: ${it.key.first}, y: ${it.key.second} => ${it.value}")
         if (it.value > 1) {
             sum++
         }
-        println()
     }
-
     println("Total overlap $sum")
 }
 
